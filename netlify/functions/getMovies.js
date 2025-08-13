9105f3687779a30852cf4c28fbc2005eaf867b13
Aviupadhyay457 <avinashupadhyay457@gmail.com>
2025-08-13 18:48:11 +0530

    Update getMovies.js

--- netlify/functions/getMovies.js
+++ netlify/functions/getMovies.js
@@ -1,6 +1,9 @@
 // import dotenv from "dotenv";
 // dotenv.config()
 
+const dotenv = require('dotenv');
+dotenv.config()
+
 const { HfInference } = require('@huggingface/inference');
 
 // import { HfInference } from '@huggingface/inference'
